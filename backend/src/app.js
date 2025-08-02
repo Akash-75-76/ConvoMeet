d9708a5b5df1ede697089d6007a4a459d6ae81a8
Akash-75-76 <deshmukhaakash752005@gmail.com>
2025-08-02 19:29:12 +0530

    database conncted socket server created

---
+++ backend/src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv"
+import { ConnectToSocket } from "./controllers/socketManager.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = 8000;
+const server=createServer(app);
+const io=ConnectToSocket(server);
+// 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({limit:"40kb",extended:true}))
+
+const dbUrl=process.env.MONGO_URL;
+async function main() {
+  await mongoose.connect(dbUrl);
+}
+
+main()
+  .then(() => {
+    console.log("Connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/home", (req, res) => {
+  res.send("Hello from Zoom Clone backend!");
+});
+
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import {Router, Router} from "express";
+
+const Router=Router();
+
+router.route("/login");
+router.route("/register");
+router.route("/add_to_activity");
+router.route("/get_all_activity");
+export default router;
