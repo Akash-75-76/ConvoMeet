31df2d8541def6653f11058706d3ede4da8aa382
Akash-75-76 <deshmukhaakash752005@gmail.com>
2025-08-05 23:17:54 +0530

    Completed landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import "../App.css"
+import { Link, useNavigate } from 'react-router-dom'
+import mobileImg from '../assets/mobile.png';
 
-export default function landing() {
-  return (
-    <div>
-      <h1>HEllo</h1>
-    </div>
-  )
+export default function Landing() {
+
+
+    const router = useNavigate();
+
+    return (
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>Apna Video Call</h2>
+                </div>
+                <div className='navlist'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
+
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
+
+                    }} role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+
+                    <p>Cover a distance by Apna Video Call</p>
+                    <div role='button'>
+                        <Link to={"/auth"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+
+                 
+                    { <img src={mobileImg} alt="Mobile" /> }
+
+
+                </div>
+            </div>
+
+
+
+        </div>
+    )
 }
